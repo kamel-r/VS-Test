b4db8d896cb8cf8b569124f883ec913652ecf2e7
GOMC <gomc@eng.wayne.edu>
2015-07-16 19:06:14 -0400

    New mvc

---
+++ WebApplication2/WebApplication2/Controllers/TestController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+namespace WebApplication2.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public string GetString()
+        {
+            return "Helloooooooooo";
+        }
+        [NonAction]
+        public string GetNothingBack()
+        {
+
+            return "hekk";
+        }
+
+        public ActionResult MyView()
+        {
+           
+
+            Employee emp = new Employee();
+
+            emp.FirstName = "Johny";
+
+          //  ViewData["Employee1"] = emp;
+
+           // ViewBag.Employee = emp;
+
+           return View("MyView",emp);
+            
+
+        }
+    }
+}
